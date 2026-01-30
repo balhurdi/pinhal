88fc2853fc04c22d6abd45667b62ca45cf3e2315
Francisco Torrinha <ftorrinha.dev@gmail.com>
2026-01-30 23:21:02 +0000

    feat: Added script to run vm

---
+++ run-qemu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+DRIVE_SIZE=10G
+MEM_VALUE=512
+ISO_PATH=iso/alpine-pinhal-3.23-x86_64.iso
+DRIVE_PATH=alpine.qcow2
+
+# Create disk
+qemu-img create -f qcow2 $DRIVE_PATH $DRIVE_SIZE
+# Run vm
+qemu-system-x86_64 -m 512 -nic user -boot once=d -cdrom $ISO_PATH -drive file=$DRIVE_PATH -display sdl -enable-kvm
