a9b04f2b86ce82f4cc522c66c3c0ad890a045491
Francisco Torrinha <ftorrinha.dev@gmail.com>
2026-01-31 15:09:28 +0000

    doc: Serializer interface

--- apps/installer/src/serializer.h
+++ apps/installer/src/serializer.h
@@ -1,5 +1,9 @@
 #pragma once
 
+/**
+ * @note Refer to https://wiki.alpinelinux.org/wiki/Using_an_answerfile_with_setup-alpine to
+ * get the list of all parameters as well as what they mean and values each field can have
+ */
 typedef struct _config_content {
     char* KEYMAPOPTS;
     char* HOSTNAMEOPTS;
@@ -17,5 +21,21 @@ typedef struct _config_content {
     char* APKCACHEOPTS;
 } config_content_t;
 
+/**
+ * @brief Creates a default config based on the config generated
+ * by the `setup-alpine -c PATH_TO_FILENAME` command
+ *
+ * @note Refer to https://wiki.alpinelinux.org/wiki/Using_an_answerfile_with_setup-alpine
+ */
 config_content_t config_content_default ();
+
+/**
+ * @brief Generates a config file compatible with the
+ * setup-alpine -f FILE_NAME command
+ *
+ * @param config A reference to a filled out config struct
+ * @param path Path to the file, in case its NULL, it outputs to stdout
+ *
+ * @note Using stdout mode is a good tool for testing
+ */
 void config_content_serialize (const config_content_t* config, const char* path);
